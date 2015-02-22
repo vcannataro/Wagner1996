be98dfb657fed504bef7ca5da5100257e59d1403
vcannataro <vcannataro@ufl.edu>
2015-02-21 19:17:01 -0500

    added some comments

--- wagner1996.R
+++ wagner1996.R
@@ -1,9 +1,10 @@
 
-N <- 10
-p <- 0.5 #0.1-0.9
-c <- 0.5#0.4-1
-generations <- 3*N
+N <- 10 #number of states in expression state vector 
+p <- 0.5 #0.1-0.9 the mean fraction of genes expressed in initial and equilibrium state 
+c <- 0.5#0.4-1 the mean fraction of connectivities different from zero 
+generations <- 3*N #generations to run the simulation
 
+#initialize gene expression pattern 
 s.0 <- rep(-1,N)
 for(i in 1:N){
   if(rbinom(1,1,p)==1){
@@ -11,6 +12,7 @@ for(i in 1:N){
   }
 }
 
+#create some optimal gene expression pattern
 s.opt <- rep(-1,N)
 for(i in 1:N){
   if(rbinom(1,1,p)==1){
@@ -18,9 +20,10 @@ for(i in 1:N){
   }
 }
 
-
+#connectivity matrix 
 w <- matrix(NA,N,N)
 
+#I decided to draw the values from a normal (like he elaborated on in the appendix). 
 for(i in 1:length(w)){
   if(runif(1)<c){
     w[i] <- round(rnorm(1,0,1),3)
@@ -29,12 +32,12 @@ for(i in 1:length(w)){
   }
 }
 
-
+#s.list stores the expression states over the generations of simulations
 s.list <- matrix(0,generations,N)
  
-s.list[1,] <- s.0  
+s.list[1,] <- s.0  #first entry is the initial pattern
   
-  
+#lets run the simulation
 for(z in 2:generations){
   
   for(i in 1:N){
@@ -58,9 +61,11 @@ for(z in 2:generations){
   }
 }
 
-
+#print the matrix showing the expression state patterns over "time" 
 s.list
 
+
+#did you hit an equilibrium? 
 identical(s.list[generations,],s.list[(generations-1),])
 
 
